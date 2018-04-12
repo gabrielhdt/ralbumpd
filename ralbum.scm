c7df508869d8b40f44a9a390117afda74d196927
gabrielhdt <gabrielhondet@gmail.com>
2018-04-12 20:34:35 +0200

    (+) Adding album implemented
    Not yet tested

--- ralbum.scm
+++ ralbum.scm
@@ -4,25 +4,27 @@
 
 (define albums (list-metadata mpd 'album))
 
-(define get-songs-of-album
-  (lambda (album)
-    (list-metadata mpd 'song 'album album)))
-
 (define choose-album
   (lambda (albums)
     (let* ([card (length albums)]
 	   [ind (random card)])
       (list-ref albums ind))))
 
+(define album-to-filelist
+  (lambda (album)
+    (let ([songprops (find-songs mpd 'album album)]
+	  [songprop-to-file (lambda (songprop)
+			      (assv 'file songprop))])
+      map songprop-to-file (songprops album))))
+
 (define add-album-plst
   (lambda (album)
-    (let ([songs (get-songs-of-album album)])
-      letrec ([loop
-		(lambda (rsongs)
-		  (if (null? rsongs)
-		    #t
-		    (begin (add-song mpd (car rsongs)) (loop (cdr rsongs)))))])
-      loop songs)))
+    letrec ([loop
+	      (lambda (rsongs)
+		(if (null? rsongs)
+		  #t
+		  (begin (add-song mpd (car rsongs)) (loop (cdr rsongs)))))])
+    loop (album-to-filelist album)))
 (display "chosen album: ")
 (display (choose-album albums))
 (newline)
