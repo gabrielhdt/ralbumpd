0647fd60ca87e4c7b2e45fd58c6867f075f9b3d5
gabrielhdt <gabrielhondet@gmail.com>
2018-04-12 18:39:14 +0200

    (+) Add all songs
    Might not work as full path might be required...

--- ralbum.scm
+++ ralbum.scm
@@ -4,11 +4,25 @@
 
 (define albums (list-metadata mpd 'album))
 
+(define get-songs-of-album
+  (lambda (album)
+    (list-metadata mpd 'song 'album album)))
+
 (define choose-album
   (lambda (albums)
     (let* ([card (length albums)]
 	   [ind (random card)])
       (list-ref albums ind))))
+
+(define add-album-plst
+  (lambda (album)
+    (let ([songs (get-songs-of-album album)])
+      letrec ([loop
+		(lambda (rsongs)
+		  (if (null? rsongs)
+		    #t
+		    (begin (add-song mpd (car rsongs)) (loop (cdr rsongs)))))])
+      loop songs)))
 (display "chosen album: ")
 (display (choose-album albums))
 (newline)
