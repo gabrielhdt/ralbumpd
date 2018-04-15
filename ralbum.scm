345697fbd20ab786d52fe6b21c3a00bc44791cec
gabrielhdt <gabrielhondet@gmail.com>
2018-04-15 12:48:42 +0200

    (+) more options
    New current album fun

--- ralbum.scm
+++ ralbum.scm
@@ -7,6 +7,14 @@
          (single-char #\a)
          (required #f)
          (value #f))
+    (next "Play next album in the playlist"
+          (single-char #\n)
+          (required #f)
+          (value #f))
+    (refill "Adds an album if playlist nearly exhausted"
+            (single-char #\r)
+            (required #f)
+            (value #f))
     (verbose "Verbose mode"
              (required #f)
              (single-char #\v)
@@ -14,11 +22,9 @@
     ))
 (define clopts (getopt-long (argv) clgrammar))
 
-; define mpd as the connection
-(define mpd (connect))
-
-; List of albums in the database
-(define albums (list-metadata mpd 'album))
+; [current-album c] returns the album of the playing song
+(define current-album
+  (lambda (conn) (cdr (assq 'Album (get-current-song conn)))))
 
 ; [choose-album a] chooses randomly an album among those in [a]
 (define choose-album
@@ -26,6 +32,12 @@
     (let* ([card (length albums)]
            [ind (random card)])
       (list-ref albums ind))))
+
+; define mpd as the connection
+(define mpd (connect))
+
+; List of albums in the database
+(define albums (list-metadata mpd 'album))
 
 ; [album-to-filelist a] returns the paths of songs (relatively to mpd
 ; database) of album [a]
@@ -71,18 +83,16 @@
     (skip-album (clear-ante-album album songprops))))
 
 ; [next-album! '()] plays the first track in
-; [behead-songprop ((album playing now) (playlist))]
+; [tonext-album ((album playing now) (playlist))]
 (define next-album!
   (lambda (conn)
-    (let* ([current-album (cdr (assq 'Album (get-current-song conn)))]
-           [rsongprops (tonext-album (current-album (get-playlist conn)))]
+    (let* ([rsongprops (tonext-album ((current-album conn)
+                                      (get-playlist conn)))]
            [next-id (cdr (assq 'Id (car rsongprops)))])
       (play-song! conn next-id))))
 
-
-
 ; Clear playlist if play state is stop. Happens when all playlist has been
-; read (avoid going back to the top.
+; read (avoid going back to the top).
 (if (eq? (cdr (assq 'state (get-status mpd))) 'stop)
   (clear-playlist! mpd)
   '())
@@ -99,5 +109,5 @@
                (newline)))
            (enqueue-songpaths songs))))
       ((pair? (assq 'next clopts))
-       (next-album! '()))
-      (else (display "No arg given")))
+       (next-album!))
+      (else (display (usage clgrammar))))
