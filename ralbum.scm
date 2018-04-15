ab8181dfdbb658608a0922e36de2924888f6a84b
gabrielhdt <gabrielhondet@gmail.com>
2018-04-15 09:55:43 +0200

    (+) began implementation of next album

--- ralbum.scm
+++ ralbum.scm
@@ -42,15 +42,50 @@
       '()
       (begin (add-song! mpd (car sgps)) (enqueue-songpaths (cdr sgps))))))
 
-(if (pair? (assq 'add clopts))
-  (let* ([chosen (choose-album albums)]
-	 [songs (album-to-filelist chosen)])
-      (begin
-	 (if (pair? (assq 'verbose clopts))
+; [behead-songprop a s] removes all songs from alist [s] appearing before
+; the album [a] and all songs of album [a].
+(define behead-songprop
+  (lambda (album songprops)
+    (letrec ([clear-ante-album
+	       (lambda (rsps)
+		 (if (eq? (assq 'album (car rsps)) album)
+		   (clear-ante-album (cdr rsps))
+		   rsps))]
+	     [skip-album
+	       (lambda (rsps)
+		 (if (eq? (assq 'album (car rsps)) album)
+		   (skip-album (cdr rsps))
+		   rsps))])
+      (skip-album (clear-ante-album songprops)))))
+
+; [next-album! '()] plays the first track in
+; [behead-songprop ((album playing now) (playlist))]
+(define next-album!
+  (lambda (conn)
+    (let* ([current-album (cdr (assq 'album (get-current-song conn)))]
+	   [rsongprops (behead-songprop (current-album (get-playlist conn)))]
+	   [next-id (cdr (assq 'id (car rsongprops)))])
+      (play-song! conn next-id))))
+      
+
+
+; Clear playlist if play state is stop. Happens when all playlist has been
+; read (avoid going back to the top.
+(if (eq? (cdr (assq 'state (get-status mpd))) 'stop)
+  (clear-playlist! mpd)
+  '())
+
+; Looks for options and triggers actions
+(cond ((pair? (assq 'add clopts))
+       (let* ([chosen (choose-album albums)]
+	      [songs (album-to-filelist chosen)])
+	 (begin
+	   (if (pair? (assq 'verbose clopts))
 	   (begin
 	     (display "Adding album: ")
 	     (display chosen)
-	     (newline))
-	 '())
-	 (enqueue-songpaths songs)))
-  '())
+	     (newline)))
+	   (enqueue-songpaths songs))))
+      ((pair? (assq 'next clopts))
+       (next-album! '()))
+      (else (display "No arg given")))
