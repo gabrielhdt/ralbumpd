9d76094606bf5f35b3e37119cd2153e087a12805
gabrielhdt <gabrielhondet@gmail.com>
2019-01-28 21:16:00 +0100

    (+) random album selection

--- ralbumpd.hs
+++ ralbumpd.hs
@@ -1,15 +1,26 @@
-import Network.MPD
+import qualified Network.MPD as MPD
+import           System.Random
+import           Control.Applicative
 
 -- |For now only refills playlist
 main :: IO ()
-main = cardAlbums >>= print
+main = randomAlbum >>= print
 
 -- |Number of albums in the database.
 cardAlbums :: IO Integer
 cardAlbums =
-  let resp = withMPD stats
+  let resp = MPD.withMPD MPD.stats
   in resp
      >>= \es -> either
                 (\_ -> return (-1))
-                (return . stsAlbums)
+                (return . MPD.stsAlbums)
                 es
+
+-- |Chooses a random album among all available
+randomAlbum :: IO (MPD.Response MPD.Value)
+randomAlbum =
+  let albums = MPD.withMPD $ MPD.list MPD.Album Nothing
+      card = cardAlbums
+      r_ind = card >>= \c -> randomRIO (0, c - 1)
+  in liftA2 (\irlv ri -> (\xs -> xs !! fromInteger ri) <$> irlv)
+     albums r_ind
