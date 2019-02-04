9d1dcfa7c5d81c28ce3968a7e9ac98cc5daef357
gabrielhdt <gabrielhondet@gmail.com>
2019-02-04 11:16:29 +0100

    (+) cabal things

--- configure.fish
+++ configure.fish
@@ -6,7 +6,7 @@ end
 set _gpp_back $GHC_PACKAGE_PATH
 set -e GHC_PACKAGE_PATH
 
-set confcmd runhaskell Setup.hs configure $flag
+set confcmd runhaskell Setup.hs configure $flag --user
 eval $confcmd
 
 set GHC_PACKAGE_PATH _gpp_back
